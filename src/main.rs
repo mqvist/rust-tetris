114319fb1ff49cd53722170f26f27dac8d3b39cb
Mika Qvist <mika.k.qvist@gmail.com>
2022-06-05 15:43:41 +0300

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -31,22 +31,22 @@ enum WallSide {
 }
 
 struct Tetromino {
-    block_deltas: [BlockPos; 4],
-    starting_pos: BlockPos,
+    blocks: [BlockPos; 4],
+    start_pos: BlockPos,
     color: Color,
 }
 
 const TETROMINOS: [Tetromino; 2] = [
     // I
     Tetromino {
-        block_deltas: [(0, 0), (0, 1), (0, 2), (0, 3)],
-        starting_pos: (20, 4),
+        blocks: [(0, 0), (0, 1), (0, 2), (0, 3)],
+        start_pos: (21, 4),
         color: LBLUE,
     },
     // J
     Tetromino {
-        block_deltas: [(1, 0), (0, 0), (0, 1), (0, 2)],
-        starting_pos: (20, 4),
+        blocks: [(1, 0), (0, 0), (0, 1), (0, 2)],
+        start_pos: (21, 4),
         color: DBLUE,
     },
 ];
@@ -89,25 +89,14 @@ fn new_wall(side: WallSide) -> SpriteBundle {
     let playfield_height = MAX_ROW as f32 * BLOCK_SIZE;
     let playfield_width = MAX_COLUMN as f32 * BLOCK_SIZE;
 
-    let (x, y, sx, sy) = match side {
+    let (x, y, width, height) = match side {
         WallSide::Left => (left_edge, 0.0, 1.0, playfield_height),
         WallSide::Right => (right_edge, 0.0, 1.0, playfield_height),
         WallSide::Top => (0.0, top_edge, playfield_width, 1.0),
         WallSide::Bottom => (0.0, bottom_edge, playfield_width, 1.0),
     };
 
-    SpriteBundle {
-        transform: Transform {
-            translation: Vec3::new(x, y, 0.0),
-            scale: Vec3::new(sx, sy, 0.0),
-            ..default()
-        },
-        sprite: Sprite {
-            color: WHITE,
-            ..default()
-        },
-        ..default()
-    }
+    new_rect(x, y, width, height, WHITE)
 }
 
 fn new_piece(mut commands: Commands) {
@@ -116,11 +105,11 @@ fn new_piece(mut commands: Commands) {
     commands
         .spawn_bundle(TransformBundle { ..default() })
         .with_children(|parent| {
-            for (r, c) in shape.block_deltas.iter() {
+            for (r, c) in shape.blocks.iter() {
                 parent
                     .spawn_bundle(new_block(
-                        shape.starting_pos.0 + *r,
-                        shape.starting_pos.1 + *c,
+                        shape.start_pos.0 + *r,
+                        shape.start_pos.1 + *c,
                         shape.color,
                     ))
                     .insert(Block);
@@ -132,10 +121,14 @@ fn new_piece(mut commands: Commands) {
 fn new_block(row: u8, col: u8, color: Color) -> SpriteBundle {
     let x = col_to_x(col);
     let y = row_to_y(row);
+    new_rect(x, y, BLOCK_SIZE, BLOCK_SIZE, color)
+}
+
+fn new_rect(x: f32, y: f32, width: f32, height: f32, color: Color) -> SpriteBundle {
     SpriteBundle {
         transform: Transform {
             translation: Vec3::new(x, y, 0.0),
-            scale: Vec3::new(BLOCK_SIZE, BLOCK_SIZE, 0.0),
+            scale: Vec3::new(width, height, 0.0),
             ..default()
         },
         sprite: Sprite { color, ..default() },
