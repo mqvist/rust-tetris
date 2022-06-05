2f187229efecaef6cf28ef36830acbb44e49c4a8
Mika Qvist <mika.k.qvist@gmail.com>
2022-06-05 13:29:38 +0300

    Add color constants

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ use bevy::{math::const_vec3, prelude::*};
 
 const BRICK_SIZE: Vec3 = const_vec3!([20.0, 20.0, 0.0]);
 const BRICK_COLOR: Color = Color::rgb(1.0, 0.2, 0.2);
+// Colors
+const CYAN: Color = Color::rgb(105.0 / 255.0, 227.0 / 255.0, 250.0 / 255.0);
+const BLUE: Color = Color::rgb(21.0 / 255.0, 2.0 / 255.0, 245.0 / 255.0);
+const ORANGE: Color = Color::rgb(238.0 / 255.0, 123.0 / 255.0, 50.0 / 255.0);
+const YELLOW: Color = Color::rgb(250.0 / 255.0, 223.0 / 255.0, 75.0 / 255.0);
+const GREEN: Color = Color::rgb(145.0 / 255.0, 250.0 / 255.0, 77.0 / 255.0);
+const RED: Color = Color::rgb(233.0 / 255.0, 55.0 / 255.0, 68.0 / 255.0);
+const MAGENTA: Color = Color::rgb(165.0 / 255.0, 34.0 / 255.0, 238.0 / 255.0);
+
 
 #[derive(Component)]
 struct Brick;
